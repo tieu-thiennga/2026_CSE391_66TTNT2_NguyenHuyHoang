8b6700a6ab47014f7f45908c5952d90204159314
tieu-thiennga <huyhoang.shogunei@gmail.com>
2026-03-05 09:13:03 +0700

    prac-3 5/3/26

---
+++ PRACTICE-3/dom.js
@@ -0,0 +1,17 @@
+const statusEl = document.getElementById("status");
+const btnHello = document.getElementById("btnHello");
+
+btnHello.addEventListener("click", function () {
+  statusEl.textContent = "Xin chào! Đây là nội dung được thay đổi bằng JavaScript.";
+});
+btnRed.addEventListener("click", function () {
+  document.body.style.backgroundColor = "red";
+});
+const nameInput = document.getElementById("nameInput");
+const greeting = document.getElementById("greeting");
+
+nameInput.addEventListener("input", function () {
+  const value = nameInput.value;
+  greeting.textContent = "Xin chào, " + value + "!";
+});
+statusEl.textContent = "Xin chào!";
